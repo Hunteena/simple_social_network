055074d572147fe0880496cfe7666add686dc26c
Nina Speranskaya <nina-sp@list.ru>
2023-07-21 12:47:17 +0600

    Add project title and description for docs

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 
-from app.routes import users
+from app.routes import users, posts
 
-app = FastAPI()
+description = """
+You are able to:
+- **view** all posts,
+- **signup**,
+- **login** and get an access token.  
+
+With the access token you are able to:
+- **create** a post,
+- **edit and delete** your own posts,
+- **like or dislike** other users' posts.
+"""
+
+app = FastAPI(
+    title="Simple social network",
+    description=description,
+    summary="RESTful API for a simple social networking application",
+)
 
 app.include_router(users.router)
